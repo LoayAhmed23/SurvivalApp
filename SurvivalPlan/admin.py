0225d6ed26dd8991b71fc2e34ab5a7bba5d1952e
LoayAhmed23 <103945328+LoayAhmed23@users.noreply.github.com>
2025-07-21 23:36:21 +0300

    Edited Admin page to include new models

--- SurvivalPlan/admin.py
+++ SurvivalPlan/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from SurvivalPlan.models import (
+    SurvivalPlan,
+    PlanItem,
+    Expense
+)
 # Register your models here.
+admin.site.register(SurvivalPlan)
+admin.site.register(PlanItem)
+admin.site.register(Expense)
